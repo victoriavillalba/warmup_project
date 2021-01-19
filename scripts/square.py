fef2d94c72cdfd9b05ed826b107a5a86ed027452
Victoria Villalba <vvillalba@scyther.cs.uchicago.edu>
2021-01-18 23:14:25 -0600

    only moves for angular velocity

--- scripts/square.py
+++ scripts/square.py
@@ -22,73 +22,55 @@ class MoveSquare(object):
 
     def process_scan(self,data):
         time = rospy.get_time()
-        print("moving")
         self.twist.linear.x = 0.1
-#        self.twist = Twist(linear=Vector3(1,0,0),angular=Vector3(0,0,0))
+        self.twist_pub.publish(self.twist)
+
         while rospy.get_time() < (time + 5.0):
             continue
         time = rospy.get_time()
-        print("turning")
         self.twist.linear.x = 0
         self.twist.angular.z = (1.570796326794897/2)
-#        self.twist = Twist(linear=Vector3(0,0,0),angular=Vector3(0,0,(1.570796326794897/2)))
-        while rospy.get_time() < (time + 1.0):
+        self.twist_pub.publish(self.twist)
+
+        while rospy.get_time() < (time + 2.0):
             continue
         time = rospy.get_time()
-        print("moving")
         self.twist.linear.y = 0.1
         self.twist.angular.z= 0
-#        self.twist = Twist(linear=Vector3(0,1,0),angular=Vector3(0,0,0))
+        self.twist_pub.publish(self.twist)
+
         while rospy.get_time() < (time + 5.0):
             continue
         time = rospy.get_time()
-        print("turning")
         self.twist.linear.y = 0
         self.twist.angular.z = (1.570796326794897/2)
-#        self.twist = Twist(linear=Vector3(0,0,0),angular=Vector3(0,0,(1.570796326794897/2)))
-        while rospy.get_time() < (time + 1.0):
+        self.twist_pub.publish(self.twist)
+        
+        while rospy.get_time() < (time + 2.0):
             continue
         time = rospy.get_time()
-        print("moving")
         self.twist.linear.x = -0.1
         self.twist.angular.z = 0
-#        self.twist = Twist(linear=Vector3(0,1,0),angular=Vector3(0,0,0))
+        self.twist_pub.publish(self.twist)
+        
         while rospy.get_time() < (time + 5.0):
             continue
         time = rospy.get_time()
-        print("turning")
         self.twist.linear.x = 0
         self.twist.angular.z = (1.570796326794897/2)
-#        self.twist = Twist(linear=Vector3(0,0,0),angular=Vector3(0,0,(1.570796326794897/2)))
-        while rospy.get_time() < (time + 1.0):
+        self.twist_pub.publish(self.twist)
+        
+        while rospy.get_time() < (time + 2.0):
             continue
         time = rospy.get_time()
-        print("moving")
         self.twist.linear.y = -0.1
         self.twist.angular.z = 0
-#        self.twist = Twist(linear=Vector3(0,1,0),angular=Vector3(0,0,0))
+        self.twist_pub.publish(self.twist)
+        
         while rospy.get_time() < (time + 5.0):
             continue
         self.twist = Twist(linear=Vector3(0,0,0),angular=Vector3(0,0,0))
         print("stopped")
-#                if (i%2 == 0):
- #                   lin = Vector3(1,0,0)
-  #                  ang = Vector3(0,0,0)
-   #             else:
-    #                lin = Vector3(1,0,0)
-     #           ang = Vector3(0,0,0)
-      #          self.twist = Twist(linear=lin,angular=ang)
-       #     time = rospy.get_time()
-        #    print("turning")
-         #   while rospy.get_time() < (time + 1.0):
-          #      lin = Vector3(0,0,0)
-           #     ang = Vector3(0,0,(1.570796326794897/2))
-            #    self.twist = Twist(linear=lin,angular=ang)
-#            i = i + 1
- #       lin = Vector3(0,0,0)
-  #      ang = Vector3(0,0,0)
-   #     self.twist = Twist(linear=lin,angular=ang)
-
         self.twist_pub.publish(self.twist)
         
 
