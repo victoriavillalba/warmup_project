40eb7982ceacad16f00b2067af1dbcd04a3250b2
Victoria Villalba <vvillalba@scyther.cs.uchicago.edu>
2021-01-18 20:24:53 -0600

    code without errors but doesn't move box

--- scripts/square.py
+++ scripts/square.py
@@ -10,21 +10,40 @@ from geometry_msgs.msg import Point
 
 class MoveSquare(object):
     def __init__(self):
-        rospy.init_node("move in a square")
+        rospy.init_node("move_in_a_square")
 
         rospy.Subscriber("/scan",LaserScan,self.process_scan)
 
         self.twist_pub = rospy.Publisher("/cmd_vel",Twist,queue_size=10)
 
-        lin = Vector3()
-        ang = Vector3()
+        lin = Vector3(0,0,0)
+        ang = Vector3(0,0,0)
         self.twist = Twist(linear=lin,angular=ang)
 
-    def proces_scan(self,data):
-        if data.ranges[0] >= distance:
-            self.twist.linear.x = 0.1
-        else:
-            self.twist.linear.x = 0
+    def process_scan(self,data):
+
+        i = 0
+        while i < 4:
+            time = rospy.get_time()
+#            my_header = Header(stamp=rospy.Time.now())
+            while rospy.get_time() < (time + 5.0):
+                if (i%2 == 0):
+                    lin = Vector3(0.1,0,0)
+                    ang = Vector3(z=0)
+                else:
+                    lin = Vector3(0,0.1,0)
+                ang = Vector3(0,0,0)
+                Twist(linear=lin,angular=ang)
+            time = rospy.get_time()
+#            my_header = Header(stamp=rospy.Time.now())
+            while rospy.get_time() < (time + 1.0):
+                lin = Vector3(0,0,0)
+                ang = Vector3(0,0,(1.570796326794897/2))
+                Twist(linear=lin,angular=ang)
+            i = i + 1
+        lin = Vector3(0,0,0)
+        ang = Vector3(0,0,0)
+        Twist(linear=lin,angular=ang)
 
         self.twist_pub.publish(self.twist)
         
